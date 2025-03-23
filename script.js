aeb89e22f283fe763446822d64b4099b0cb41c8c
Baumann001 <gustavo_baumann@estudante.sesisenai.org.br>
2025-03-23 15:56:20 -0300

    add-espaçamento

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 function verificarSinal(numero){
    if (numero > 0) {
-    console.log(numero + "é positivo!");
+    console.log(numero + " é positivo!   " );
    } else if (numero < 0 ) {
-        console.log(numero + "é negativo");
+        console.log(numero + " é negativo "  );
    } else {
-    console.log(numero+"é zero!");
+    console.log(numero + " é zero!  " );
    }
 }
 
-let numero= parseFloat(prompt("Digite um número:"));
+let numero = parseFloat(prompt("Digite um número:"));
 
 verificarSinal(numero);
