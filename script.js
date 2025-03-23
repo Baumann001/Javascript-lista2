de75302133082cf94c901a918a8ec5c58f4abf91
Baumann001 <gustavo_baumann@estudante.sesisenai.org.br>
2025-03-23 16:31:59 -0300

    add- questao 13

--- script.js
+++ script.js
@@ -31,3 +31,14 @@ let temperaturaFahrenheit = converterTemperatura(temperaturaCelsius);
 console.log(`A temperatura em Fahrenheit é: ${temperaturaFahrenheit}`);
 */
 
+let soma=0;
+let numero=1;
+
+
+while(numero<100){
+   soma += numero;
+   numero ++;
+}
+console.log("A soma dos números de 1 a 100 é:", soma);
+
+
