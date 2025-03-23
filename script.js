15399ed6bb0fc637a28819ce65a0453d90f90c99
Baumann001 <gustavo_baumann@estudante.sesisenai.org.br>
2025-03-23 17:33:10 -0300

    add-  correcao vírgula

--- script.js
+++ script.js
@@ -94,3 +94,16 @@ let fraseCompleta= saudacao(digitaNome);
 
 console.log(fraseCompleta);
 */
+
+function verificarAprovação(nota){
+   if (nota>=6);
+      console.log("Voce foi aprovado");
+
+   }else{
+      console.log("Voce foi reprovado");
+   }
+
+   let aprovacao= parsefloat(prompt("Digite sua nota:"));
+
+   let verificarAprovacao(aprovacao);
+   
