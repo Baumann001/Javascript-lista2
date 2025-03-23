7c5ce47012e618344e38f43b42f68fd7d7dea4c5
Baumann001 <gustavo_baumann@estudante.sesisenai.org.br>
2025-03-23 16:16:19 -0300

    add- questaõ 12 return

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+/*
+Atividade 11
 function verificarSinal(numero){
    if (numero > 0) {
     console.log(numero + " é positivo!   " );
@@ -11,3 +13,17 @@ function verificarSinal(numero){
 let numero = parseFloat(prompt("Digite um número:"));
 
 verificarSinal(numero);
+*/
+
+
+function converterTemperatura(celsius){
+   return ( celsius * 9/5   ) + 32;
+
+}
+
+let temperaturaCelsius = parseFloat(prompt("Digite a temperatura em celsius:"));
+
+let temperaturaFahrenheit = converterTemperatura(celsius);
+
+console.log(`A temperatura em Fahrenheit é: ${temperaturaFahrenheit}`);
+
